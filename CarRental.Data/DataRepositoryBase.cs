3bf129c13f4bd3d777dc77b01e137cf4a81e05f6
Salle79 <salle79@gmail.com>
2015-05-27 10:32:02 +0200

    ggg

--- CarRental.Data/DataRepositoryBase.cs
+++ CarRental.Data/DataRepositoryBase.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Core.Common.Contracts;
 using Core.Common.Data;
+using System.Data;
+using System.Data.Entity;
 
 namespace CarRental.Data
 {
